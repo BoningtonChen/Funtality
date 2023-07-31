0bbe4b25896836414a233fa74c6cb13798bc395f
Bonington Chen <yikun.chen@163.com>
2023-07-31 18:07:28 +0800

    添加vector和buffer帮助实现文件，测试vector可行性

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include "helpers/vector.h"
 
 int main()
 {
-	printf("Hello!\n");
+	struct vector* vec = vector_create(sizeof(int));
+	int x = 50;
+	vector_push(vec, &x);
+	x = 20;
+	vector_push(vec, &x);
+
+	vector_set_peek_pointer(vec, 0);
+	int* ptr = vector_peek(vec);
+	while (ptr)
+	{
+		printf("%i\n", *ptr);
+		ptr = vector_peek(vec);
+	}
+	
 	return 0;
 }
